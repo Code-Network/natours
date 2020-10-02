4bc844fef22aaa37eea3cda6289251560725e6c8
Koko-Dev <koko.webdev@gmail.com>
2020-10-02 12:17:58 -0400

    Some refactoring in models/tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,31 +1,30 @@
 const mongoose = require('mongoose');
 
 //
-// ----------------------------------------------------------------
+// -------------------------------------------------------
 // TODO: ----------------- Create a Schema -------------
-// ----------------------------------------------------------------
-//
+// -------------------------------------------------------
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price']
-  }
+    required: [true, 'A tour must have a price'],
+  },
 });
 
 //
 // ------------------------------------------------------------------
 // TODO: ----------------- Create a Model ---------------------------
 // ------------------------------------------------------------------
-//
+
 // Always use uppercase on Model Variables
 const Tour = mongoose.model('Tour', tourSchema);
 

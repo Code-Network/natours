72f34ee4b16ec5fade4ec9278bf23dd2e803f155
Koko-Dev <koko.webdev@gmail.com>
2020-10-07 14:17:37 -0400

    In tourController.js, in getAllTours(), refactor and add comments to 2) Sorting section

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -66,17 +66,19 @@ exports.getAllTours = async (req, res) => {
     // TODO:  2) Sorting
     // Use sort() to organize the queries by price values in ascending order
     // But if there is a tie in price, then we must sort them by a second criteria.
-    // In Mongoose: sort('price ratingAverage')
-    // Use a comma for second criteria: localhost:3000/api/v1/tours?sort=price,ratingsAverage
+    // In Mongoose: sort('price ratingAverage'),
+    //    or sort('price -ratingAverage') for highest ratingsAverage first in a price tie
+    // Use a comma for second criteria:
+    //    localhost:3000/api/v1/tours?sort=price,ratingsAverage
+    //    localhost:3000/api/v1/tours?sort=price,-ratingsAverage
     // We will then have to replace the comma with a space
     // Ex. localhost:3000/api/v1/tours?sort=price
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy); // price ratingsAverage
-      // query = query.sort(req.query.sort);
+      // console.log(sortBy); // price ratingsAverage
       query = query.sort(sortBy);
     }
-    // 3) Sorting
+
     // TODO:  EXECUTE THE QUERY
     const tours = await query;
 

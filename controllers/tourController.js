092dbf20f6fb3603b92feae2c2372d0d5bf52d84
Koko-Dev <koko.webdev@gmail.com>
2020-10-09 13:52:18 -0400

    In tourController.js, add a getTourStats() handler/controller to match ratingsAverage of documents >= 4.5 and group them by some new fields which calculate the price average, min, max and ratingsAverage

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -193,3 +193,37 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+//
+// -----------------------------
+//
+
+// TODO:  f.  Get Tour Stats Handler / Controller
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: stats,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};

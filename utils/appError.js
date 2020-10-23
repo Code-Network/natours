13b120d7b3eae603f8fe7a1e85182723bc8799b2
Koko-Dev <koko.webdev@gmail.com>
2020-10-23 11:09:30 -0400

    In utils/appError.js, create a class that handles all status code 400s and 500s errors

---
+++ utils/appError.js
@@ -0,0 +1,21 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    // message is the only parameter the built-in Error accepts
+    super(message);
+    this.statusCode = statusCode;
+
+    // If the statusCode starts with a 4 it is 'fail',
+    //    if not then it is 500, 'error'
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+    // This is for testing purposes because sometimes you get some
+    //   crazy errors that does not come from us, so if the error has
+    //   this.isOperational set to true, then we know it is ours/legit
+    this.isOperational = true;
+
+    // Capture the stack trace
+    // this => The current object
+    // this.constructor => The AppError class itself
+    Error.captureStackTrace(this, this.constructor);
+  }
+}

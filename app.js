d0c0bbee6c9979d7029d166ba5fa1354b2132189
Koko-Dev <koko.webdev@gmail.com>
2020-10-23 12:19:53 -0400

    In app.js, move the Global Error Handler to the new controllers/errorController.js file and import; export the global handler into app.js in var globalErrorHandler and substitute that var name into app.use

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -119,37 +120,7 @@ app.all('*', (req, res, next) => {
 });
 
 // TODO:  Define a GLOBAL Error Handling Middleware
-app.use((err, req, res, next) => {
-  // See what is on the stack trace
-  // The stack shows us where the error happened
-  console.log('This is the STACK TRACE', err.stack);
-
-  /* DEFAULT STATUS CODE
-   -- We want to read the err status code from the object itself.
-   -- When we create the status code on res.status,
-      we will define the status code on the status error.
-   -- Error Code 500 = Internal Server Error
-   -- We will define a default (500)
-   -- Ideally, we want to read the Error Object err.statusCode,
-   because there will be errors not coming from us, but from the
-   application, errors that we have not defined ourselves.
-   -- But if it is not defined, our default will be 500.
-   */
-  err.statusCode = err.statusCode || 500;
-
-  /*
-      DEFINE THE STATUS PROPERTY (i.e. status: 'fail')
-      We also want to define the value of the status property:.
-      Again we want to read the status property ( ex. 'fail' ),
-      but it will be 'error' by default if status is undefined.
-  */
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 //
 // =================================================================
---
+++ controllers/errorController.js
@@ -0,0 +1,33 @@
+// TODO:  Define a GLOBAL Error Handling Middleware
+
+module.exports = (err, req, res, next) => {
+  // See what is on the stack trace
+  // The stack shows us where the error happened
+  console.log('This is the STACK TRACE', err.stack);
+
+  /* DEFAULT STATUS CODE
+	 -- We want to read the err status code from the object itself.
+	 -- When we create the status code on res.status,
+	 we will define the status code on the status error.
+	 -- Error Code 500 = Internal Server Error
+	 -- We will define a default (500)
+	 -- Ideally, we want to read the Error Object err.statusCode,
+	 because there will be errors not coming from us, but from the
+	 application, errors that we have not defined ourselves.
+	 -- But if it is not defined, our default will be 500.
+	 */
+  err.statusCode = err.statusCode || 500;
+
+  /*
+	 DEFINE THE STATUS PROPERTY (i.e. status: 'fail')
+	 We also want to define the value of the status property:.
+	 Again we want to read the status property ( ex. 'fail' ),
+	 but it will be 'error' by default if status is undefined.
+	 */
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};

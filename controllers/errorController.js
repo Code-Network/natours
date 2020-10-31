04195d9a3662a979418c666fe3c348384ba4d625
Koko-Dev <koko.webdev@gmail.com>
2020-10-31 18:17:52 -0400

    In errorController.js, in module.exports, write a comment that explains that a CastError is sent by Mongoose when a bad URL is requested, i.e. localhost:3000/api/v1/tours/apple

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -65,7 +65,9 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
 
+    // Mongoose sends a CastError when a wrong URL is sent i.e. /apple
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);

c973684062ae5ccf2e0c13c8f3385610f0deecd5
Koko-Dev <koko.webdev@gmail.com>
2020-11-01 15:49:41 -0500

    Create userModel.js in models directory and create a userSchema with fields name, email, photo, password and password confirm; use npm validator to validate email; create the model and export Users

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'A password must be at least 8 chars long']
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    minlength: [8, 'A password must be at least 8 chars long']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

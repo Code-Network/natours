a2b2c1766b14f122ec0a4aafdb5100237e3ebe44
Koko-Dev <koko.webdev@gmail.com>
2020-11-16 09:14:42 -0500

    In authController, in exports.signup, add a comment explaining that to create a new document based on the model in userModel.js, the newUser data is in req.body

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,8 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
+  // Create a new document based on the model
+  // The data is in req.body
   const newUser = await User.create(req.body);
 
   res.status(201).json({

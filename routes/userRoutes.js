9afea6f84a81eddc288039c819138cbab4a984bc
Koko-Dev <koko.webdev@gmail.com>
2020-11-18 14:37:40 -0500

    In userRoutes.js, create a POST route for /login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // ex. localhost:3000/api/v1/users/signup POST Request
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 router
   .route('/')

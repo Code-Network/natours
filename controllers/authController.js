52a9ab67d37c32d7f64a7f8aecb2f65a5034ce68
Koko-Dev <koko.webdev@gmail.com>
2020-11-18 09:58:32 -0500

    In authController.js, fix security flaw when creating a new user in signup; req.body would allow a user to register as admin; instead itemize the specific properties newUser would have access to

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,16 @@ const catchAsync = require('./../utils/catchAsync');
 exports.signup = catchAsync(async (req, res, next) => {
   // Create a new document based on the model
   // The data is in req.body
-  const newUser = await User.create(req.body);
+  // Note: Serious security flaw because using req.body user can register as admin
+  // const newUser = await User.create(req.body);
+
+  // TODO: Security fix for creating a new user
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
 
   res.status(201).json({
     status: 'success!',

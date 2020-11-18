b8ba28060d4d3c108ecc6f5d60bf1e963d1beec8
Koko-Dev <koko.webdev@gmail.com>
2020-11-18 15:21:00 -0500

    In userController.js, in exports.getAllUsers, surround function with catchAsync to take care of the error handling replacing res.status(500)... ; define const users to await until it finds and returns to the client all documents from the Users collection; at this point, since in userModel.js userSchema.password.select is set to false, the password will not be displayed either in Postman or the MongoDB database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,25 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users
+    }
   });
-};
+
+  /*
+      Replace the following with catchAsync();
+
+      res.status(500).json({
+       status: 'error',
+       message: 'This route is not yet defined!'
+       });
+ */
+});
 
 // This error message fires here:
 //  localhost:3000/api/v1/signup

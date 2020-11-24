70dcbcbfab9001b48d12857d0b7bdf28c62a7a64
Koko-Dev <koko.webdev@gmail.com>
2020-11-24 12:35:57 -0500

    In authController.js, add exports.protect catchAsync with async function parameter; define let token and if statement which conditional which sees if there is req.headers.authorization && req.headers.authorization.startsWith('Bearer'); if the conditional is true, set req.headers.authorization.split(' ')[1] equal to token; this will be the jwt string

--- controllers/authController.js
+++ controllers/authController.js
@@ -122,3 +122,45 @@ exports.login = catchAsync(async (req, res, next) => {
     token
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // The token will be the second part of the string
+    // split() turns it into an array and we use the second element
+    //    which would be the token, eventually
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  // if (!token) {
+  //   return next(
+  //     new AppError('You are not logged in! Please log in to get access.', 401)
+  //   );
+  // }
+
+  // 2) Validate the token - Verification token
+  // const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  //
+  // 3) Check if user still exists
+  // const currentUser = await User.findById(decoded.id);
+  // if (!currentUser) {
+  //   return next(
+  //     new AppError('The user belonging to this token no longer exists', 401)
+  //   );
+  // }
+  //
+  // 4) Check if user changed password after the token/JWT was issued
+  // if (currentUser.changedPasswordAfter(decoded.iat)) {
+  //   return next(
+  //     new AppError('User recently changed password!  Please log in again', 401)
+  //   );
+  // }
+  //
+  // // GRANT ACCESS TO PROTECTED ROUTE
+  // req.user = currentUser;
+  next();
+});

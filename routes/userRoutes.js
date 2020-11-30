ec2609f3f308303f23f8ded8fe8f479bc7062983
Koko-Dev <koko.webdev@gmail.com>
2020-11-30 16:37:35 -0500

    In userRoutes.js, in router.route('/'), remove authController.protect from before userController.getAllUsers; we remove it because we do not want all users to have access to getAllUsers at this time, possibly ever unless they have admin status; this may change in future

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ router.post('/login', authController.login);
 
 router
   .route('/')
-  .get(authController.protect, userController.getAllUsers)
+  .get(userController.getAllUsers)
   .post(userController.createUser);
 
 router

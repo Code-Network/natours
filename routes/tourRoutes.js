353eb76aac7c13aa3634e1cffecd43d5eab91f6d
Koko-Dev <koko.webdev@gmail.com>
2020-12-03 12:56:16 -0500

    In tourRoutes.js, since the retrictTo() function has not yet been implemented, comment it out to test userSchema role property

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -24,7 +24,7 @@ router
   .patch(tourController.updateTour)
   .delete(
     authController.protect,
-    authController.restrictTo('admin'),
+    // authController.restrictTo('admin'),
     tourController.deleteTour
   );
 

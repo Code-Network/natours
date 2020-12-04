41b4c2b97c6f1cb64bbe7a5f142ee81ed95ba3ff
Koko-Dev <koko.webdev@gmail.com>
2020-12-04 14:36:04 -0500

    In utils/email.js, in sendEmail, make the function async and await sending the email; module exports sendEmail

--- utils/email.js
+++ utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = options => {
-  // 1) Create a transporter
+const sendEmail = async options => {
+  // TODO: 1) Create a transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
@@ -11,14 +11,28 @@ const sendEmail = options => {
     }
   });
 
-  // 2) Define the email options
+  // TODO: 2) Define the email options
   const mailOptions = {
     from: 'Bree Lorenz admin@jonas.io',
     to: options.email,
     subject: options.subject,
     text: options.message
     // html:
-
-    // 3) Actually send the email with nodemailer
   };
+
+  /*
+     3) TODO: Actually send the email with nodemailer sendMail()
+      -- sendMail() syntax from nodemailer returns a PROMISE,
+            so we will async/await instead
+
+      Mail.sendMail(
+          data: Object,
+          callback?: Function)
+      // Sends an email using the preselected transport object
+      // Params: data -- E-data description
+                 callback - Callback to run once the sending succeeded or failed
+  */
+  await transporter.sendMail(mailOptions);
 };
+
+module.exports = sendEmail;

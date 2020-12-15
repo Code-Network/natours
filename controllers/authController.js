e18450bf5fc8c60bd970676950be2c8072745422
Koko-Dev <koko.webdev@gmail.com>
2020-12-15 14:47:41 -0500

    In authController.js, in var createSentToken, in var cookieOptions, set httpOnly to true; this will make it so that the cookie cannot be accessed or modified in any way by the Browser (prevents cross-site scripting attacks) so that all that the Browser would be able to do is to receive the cookie, store it, and then send it automatically along with every request

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,8 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    )
+    ),
+    httpOnly: true
   };
 
   // todo: Only set cookie option property secure:true in production

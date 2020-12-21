2cf77f63829ffba139ed59d924697dd62256edf8
Koko-Dev <koko.webdev@gmail.com>
2020-12-21 15:15:11 -0500

    In reviewController.js, in exports.createReview, we want to allow for Nested Routes; make it so that the user can manually specify tour and user ID by defining the tour and user who are currently logged in when they are not specified in the request body; When tour id and user id are not specified, tour comes from the URL: '{{URL}}apiv1/tours/5c88fa8cf4afda39709c2951/reviews' and the user id would come from authController.protect
    The two if statements ensure that we have correct user id and tour id:
    
    exports.createReview = catchAsync(async (req, res, next) => {
      // TODO: Allow Nested Routes
      // This will make it so that the user can manually specify tour and user ID.
      // Define tour and user when they are not specified in the Request Body.
      // We just created the route for /tour/:tourId/reviews so we have to
      //  let the app know we want to use the current tour and the current user
      // So, if we didn't specify the tourId in the body,
      //    then we want to define that as the one coming from the URL
      if (!req.body.tour) req.body.tour = req.params.tourId;
    
      // Note: We get req.user from the .protect() middleware
      if (!req.body.user) req.body.user = req.user.id;
    
      const newReview = await Review.create(req.body);
    
      res.status(201).json({
        status: 'success',
        data: {
          review: newReview
        }
      });
    });

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,6 +14,18 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // TODO: Allow Nested Routes
+  // This will make it so that the user can manually specify tour and user ID.
+  // Define tour and user when they are not specified in the Request Body.
+  // We just created the route for /tour/:tourId/reviews so we have to
+  //  let the app know we want to use the current tour and the current user
+  // So, if we didn't specify the tourId in the body,
+  //    then we want to define that as the one coming from the URL
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+
+  // Note: We get req.user from the .protect() middleware
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({

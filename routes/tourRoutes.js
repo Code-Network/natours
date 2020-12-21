df8c553d82bbe79e6bdf20ed8302bc34a38810f5
Koko-Dev <koko.webdev@gmail.com>
2020-12-21 14:18:02 -0500

    In tourRoutes.js, add '/:tourId/reviews' POST route to implement an endpoint such as /tour/someTourId/reviews used for an authenticated user with a role of 'user' to post a review of a specific tour
    router('/:tourId/reviews').post(
      authController.protect,
      authController.restrictTo('users'),
      reviewController.createReview
    );
    
    NOTE: Although it doesn't make much sense to put a reviews route in tourRoutes.js, we will do this for now because the route starts with /tour
    ex. // POST /tour/5c88fa8cf4afda39709c2955/reviews

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -29,4 +29,14 @@ router
     tourController.deleteTour
   );
 
+// POST /tour/26544/reviews
+// GET /tour/26544/reviews
+// GET /tour/26544/reviews/98745552
+
+router('/:tourId/reviews').post(
+  authController.protect,
+  authController.restrictTo('users'),
+  reviewController.createReview
+);
+
 module.exports = router;

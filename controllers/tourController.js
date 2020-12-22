7856b7ea77741d757ade772139ebaaf29f8f3f3b
Koko-Dev <koko.webdev@gmail.com>
2020-12-22 13:09:34 -0500

    In tourController.js, comment out exports.deleteTour and instead call deleteOne from the factoryHandler.js file and pass the Tour model
    --
    -- So, we go to this:
    --
    exports.deleteTour = factory.deleteOne(Tour);
    
    --
    -- From this:
    --
    exports.deleteTour = catchAsync(async (req, res, next) => {
      const tour = await Tour.findByIdAndDelete(req.params.id);
    
      if (!tour) {
        return next(new AppError('No tour found with that ID', 404));
      }
    
      res.status(204).json({
        status: 'success',
        data: null
      });
    });
    
    --
    -- Using this from handlerFactory.js:
    --
    exports.deleteOne = Model =>
      catchAsync(async (req, res, next) => {
        const doc = await Model.findByIdAndDelete(req.params.id);
    
        if (!doc) {
          return next(new AppError('No document found with that ID', 404));
        }
    
        res.status(204).json({
          status: 'success',
          data: null
        });
      });

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -90,18 +90,20 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = factory.deleteOne(Tour);
 
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-});
+// exports.deleteTour = catchAsync(async (req, res, next) => {
+//   const tour = await Tour.findByIdAndDelete(req.params.id);
+//
+//   if (!tour) {
+//     return next(new AppError('No tour found with that ID', 404));
+//   }
+//
+//   res.status(204).json({
+//     status: 'success',
+//     data: null
+//   });
+// });
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([

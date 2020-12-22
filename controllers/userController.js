21903dd1975a4c23043fbccb411c3273380650a3
Koko-Dev <koko.webdev@gmail.com>
2020-12-22 15:45:26 -0500

    In userController.js, use the exports.updateOne function from handlerFactory.js to modify exports.updateUser; pass the User model in as a parameter; NOTE: Do not use this is update passwords! This should only be for admins and we will have to modify the route to ensure only admins can update the user
    In handlerFactory.js, we will use the updateOne function
    --
    exports.updateOne = Model =>
      catchAsync(async (req, res, next) => {
        const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
          new: true,
          runValidators: true
        });
    
        if (!doc) {
          return next(new AppError('No document found with that ID', 404));
        }
    
        res.status(200).json({
          status: 'success',
          data: {
            data: doc
          }
        });
      });
    
      --
      -- In userController.js, we will replace exports.updateUser with this:
      --
        exports.updateUser = factory.updateOne(User);

--- controllers/userController.js
+++ controllers/userController.js
@@ -96,12 +96,15 @@ exports.createUser = (req, res) => {
 };
 
 // Admins Only:  to update user
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
+// NOTE: Do NOT update passwords with this!
+exports.updateUser = factory.updateOne(User);
+
+// exports.updateUser = (req, res) => {
+//   res.status(500).json({
+//     status: 'error',
+//     message: 'This route is not yet defined!'
+//   });
+// };
 
 // exports.deleteUser = (req, res) => {
 //   res.status(500).json({

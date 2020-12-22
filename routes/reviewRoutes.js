9ac0f49e69c9a511222e6641007f2dd85dc69942
Koko-Dev <koko.webdev@gmail.com>
2020-12-22 11:50:18 -0500

    In reviewRoutes.js, in const router, set express.Router options { mergeParams: true } in order to gain access to tourRoute's :tourId params
    const router = express.Router({ mergeParams: true });

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+// Set some mergeParams:true in Router to allow access to params tourId
+//    from the tour router. Setting mergeParams to true enables access
+//    to the tour router's tourId param
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')

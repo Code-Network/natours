2f22bf4e8b11960e2b8fd248ab6fb4d5132f427e
Koko-Dev <koko.webdev@gmail.com>
2020-12-28 09:29:08 -0500

    In reviewController.js, comment out var catchAsync because it is now no longer in use; perhaps we will use it in the future

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const Review = require('./../models/reviewModel');
-const catchAsync = require('./../utils/catchAsync');
+
+// const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getAllReviews = factory.getAll(Review);

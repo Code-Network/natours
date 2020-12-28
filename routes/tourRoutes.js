ed9069644ef79bfb8312de4f1faf448581e7ef89
Koko-Dev <koko.webdev@gmail.com>
2020-12-28 13:21:45 -0500

    In tourRoutes.js, in the '/monthly-plan/:year' GET route, authenticate users and restrict tourController.getMonthlyPlan to admin, lead-guide and guide
    router
      .route('/monthly-plan/:year')
      .get(
        authController.protect,
        authController.restrictTo('admin', 'lead-guide', 'guide'),
        tourController.getMonthlyPlan
      );

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -37,7 +37,14 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
 
 router
   .route('/')

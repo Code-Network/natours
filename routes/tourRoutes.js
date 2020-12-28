1f1acd829acbf800724d5664bf4addcfa98f4842
Koko-Dev <koko.webdev@gmail.com>
2020-12-28 13:06:40 -0500

    In tourRoutes.js, in POST route '/', to createTour, we want only admins and lead-guides to have the ability to create a tour
    router
      .route('/')
      .get(tourController.getAllTours)
      .post(
        authController.protect,
        authController.restrictTo('admin', 'lead-guide'),
        tourController.createTour
      );

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -42,7 +42,11 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 
 router
   .route('/:id')

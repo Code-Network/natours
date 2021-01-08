0652abde28a77529805a160e66b9002ca154625d
Koko-Dev <koko.webdev@gmail.com>
2021-01-08 14:09:12 -0500

    In viewsController, import the Tour Model and delineate steps torendering overview page in exports.getOverview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,12 @@
+const Tour = require('./../models/tourModel');
+
 exports.getOverview = (req, res) => {
+  // 1) Get tour data from collection
+
+  // Build template
+
+  // Render that template using tour data from 1)
+  //  -- so first import the Tour Model above
   res.status(200).render('overview', {
     title: 'All Tours'
   });

93cd4c5e081f232836d51f83843c7aa289426bcf
Koko-Dev <koko.webdev@gmail.com>
2021-01-07 21:24:00 -0500

    Create routes/viewRoutes.js and move all of the PUG routes, '/', '/tour', '/overview', there; import express and create an express Router and export it

--- app.js
+++ app.js
@@ -142,54 +142,24 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 
-// Create a route from which we will access the base.pug template
-// We generally use app.get() when rendering pages in the browser.
-// URL is the route we will use, which will be the root of our website => '/'
-// Handler function is (req, res) => {}
-app.get('/', (req, res) => {
-  // Set status to 200 and render base pug; no need to specify .pug
-  // tour and user are locals in the pug file
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
-  });
-});
-
-// PUG ROUTES
-// Create /overview route in get() and create overview.pug (in render)
-//    and render passing local - title
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-// Create /tour route in get() and create tour.pug (in render)
-//    and render passing local - title
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  });
-});
-
 // COLLECTION ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 
-// ============================================================================
-// ============================================================================
+// ====================================================================
+// ====================================================================
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-// ============================================================================
-// ============================================================================
+// =====================================================================
+// =====================================================================
 
 app.use(globalErrorHandler);
 
-// ============================================================================
-// ============================================================================
+// =====================================================================
+// =====================================================================
 
 module.exports = app;

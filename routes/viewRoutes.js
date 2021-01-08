b7ce1a68cdefc0f5aa548b195339c43ebfffa16a
Koko-Dev <koko.webdev@gmail.com>
2021-01-07 22:14:43 -0500

    In viewRoutes.js, import the viewsController.js file and create a route '/' which will point to a getOverview function in the viewsController.js file which will render the overview.pug page; also create a '/tour' route which will point to a getTour function in the viewsController.js file; remove all other routes from the viewRoutes.js file and test
    // All tests to localhost:3000/tour and localhost:3000 nominal
    // In viewRoutes.js
    const express = require('express');
    const viewsController = require('./../controllers/viewsController');
    const router = express.Router();
    router.get('/', viewsController.getOverview);
    router.get('/tour', viewsController.getTour);
    module.exports = router;
    
    // In viewController.js
    exports.getOverview = (req, res) => {
      res.status(200).render('overview', {
        title: 'All Tours'
      });
    };
    
    exports.getTour = (req, res) => {
      res.status(200).render('tour', {
        title: 'The Forest Hiker Tour'
      });
    };

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,35 +1,9 @@
 const express = require('express');
+const viewsController = require('./../controllers/viewsController');
 
 const router = express.Router();
 
-// PUG ROUTES
-// Create a route from which we will access the base.pug template
-// We generally use .get() when rendering pages in the browser.
-// URL is the route we will use, which will be the root of our website => '/'
-// Handler function is (req, res) => {}
-router.get('/', (req, res) => {
-  // Set status to 200 and render base pug; no need to specify .pug
-  // tour and user are locals in the pug file
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
-  });
-});
-
-// Create /overview route in get() and create overview.pug (put in render)
-//    and render passing local - title
-router.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-// Create /tour route in get() and create tour.pug (put in render)
-//    and render passing local - title
-router.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
-  });
-});
+router.get('/', viewsController.getOverview);
+router.get('/tour', viewsController.getTour);
 
 module.exports = router;

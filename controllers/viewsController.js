370b46fc9b52480a803a228a98c633e69a63fc98
Koko-Dev <koko.webdev@gmail.com>
2021-01-14 14:57:39 -0500

    In vewsController.js, there is an error I simply cannot find, so I retyped the entire thing, only modifying text. Kept getting an error which suggested there were two competing responses; error resloves

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,20 +1,16 @@
-const Tour = require('./../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // Build template
-
-  // Render that template using tour data from 1)
-  //  -- so first import the Tour Model above
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours: tours
+    tours
   });
-
-  next();
 });
 
 exports.getTour = (req, res) => {

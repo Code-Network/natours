436d4aba7d276ba6c843829e425cc5f0fedd12c4
Koko-Dev <koko.webdev@gmail.com>
2021-01-25 09:56:52 -0500

    In public/js/mapbox.js, add a comment explaining why the script tag pointing to mapbox.js must be added to the end of base.pug instead of at the block append head of tour.pug, i.e. The DOM had not been loaded at tour.pug block append head, so we must place the script tag poining to mapbox.js at the end of base.pug

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,21 @@
 /* eslint-disable */
 
-console.log('hello from the client side :P');
+/*
+ Get location data
+
+ -- Ran into an error where const locations returned an
+ Uncaught TypeError: Cannot read property 'dataset' of null
+
+    because the script script(src='/js/mapbox.js')
+ was integrated at the beginning of tour.pug while it
+ really should be at the bottom of the tour.pug page
+ because the DOM is not yet loaded in the beginning.
+
+ -- To fix this error, we remove the script tag from tour.pug
+ and put it under the footer in base.pug
+
+ -- Note:  PUG files do not trigger a restart of our server
+ */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
 console.log(locations);

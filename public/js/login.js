41c664c778ba869811764d4946309cecc54ea78c
Koko-Dev <koko.webdev@gmail.com>
2021-01-26 09:07:34 -0500

    In public/js/login.js, use axios to perform an HTTP POST request to our backend login URL; the payload will be the email and password the user put in when the form submit link was fired; because axios returns a Promise, turn the login function into an async function and await the axios request
    const login = async (email, password) => {
      await axios({
        method: 'POST',
        url: 'http://127.0.0.1:3000/api/v1/users/login',
        data: {
          email: email,
          password: password
        }
      });
    };

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,14 @@
 /* eslint-disable */
 // Create login function
-const login = (email, password) => {
-  console.log(email, password);
+const login = async (email, password) => {
+  await axios({
+    method: 'POST',
+    url: 'http://127.0.0.1:3000/api/v1/users/login',
+    data: {
+      email: email,
+      password: password
+    }
+  });
 };
 
 document.querySelector('.form').addEventListener('submit', e => {

5ca09147d03dc2460b7cc104f574d7a7957a7fbd
Koko-Dev <koko.webdev@gmail.com>
2021-01-26 16:40:07 -0500

    For testing only, in routes/viewRoutes.js, protect the getTour page; once it works, restore to former

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 
+// const authController = require('./../controllers/authController');
+
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
+
+// Test Only: protect getTour and test by removing cookie
+// router.get('/tour/:slug', authController.protect, viewsController.getTour);
+
 router.get('/tour/:slug', viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
 

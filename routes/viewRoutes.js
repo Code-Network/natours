1e2349e3560a8f1a4f92fd49d115ae3176f05d88
Koko-Dev <koko.webdev@gmail.com>
2021-01-27 11:17:57 -0500

    In routes/viewRoutes.js, explain that we use router.use(authController.isLoggedIn) in order to make available this function to all routes in that file; so currently, the GET routes it is available to is '/', '/tour/:slug' and '/login'; remember that the goal is to eventually display logged in user account, including a logout link, on the top right of every page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,9 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+// Apply isLoggedIn to every single route we create on this page
 router.use(authController.isLoggedIn);
+
 router.get('/', viewsController.getOverview);
 
 // Test Only: protect getTour and test by removing cookie

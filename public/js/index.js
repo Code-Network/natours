da2d0c681d2c0c4826d765489678a71a0ecb2065
Koko-Dev <koko.webdev@gmail.com>
2021-01-29 10:16:24 -0500

    NPM install '@babel/polyfill' into project and import as first imported item into public/js/index.js; this package has been deprecated in vavor of directly including core-js/stable ( to polyfill ECMAScript (features) ) and regenerator-runtime/runtime ( needed to use transpiled generator functions ); Babel includes a polyfill that includes a custom regenerator runtime and core-js; but, for now, although we should not really use this library tool and we should just use the new built-ins like Promise or WeakMap, Array.from, Object.assign, etc.. I have not looked into it deeply enough and will stick to the library but create an issue so that I can modify this at some later date

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,28 @@
 /* eslint-disable */
+import '@babel/polyfill';
+import { display, displayMap } from './mapbox';
 import { login } from './login';
+
+/*
+ Get location data
+
+ -- Ran into an error where const locations returned an
+ Uncaught TypeError: Cannot read property 'dataset' of null
+
+ because the script script(src='/js/mapbox.js')
+ was integrated at the beginning of tour.pug while it
+ really should be at the bottom of the tour.pug page
+ because the DOM is not yet loaded in the beginning.
+
+ -- To fix this error, we remove the script tag from tour.pug
+ and put it under the footer in base.pug
+
+ -- Note:  PUG files do not trigger a restart of our server
+ */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+displayMap(locations);
 
 document.querySelector('.form').addEventListener('submit', e => {
   e.preventDefault();
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,78 +1,69 @@
 /* eslint-disable */
 
 /*
- Get location data
-
- -- Ran into an error where const locations returned an
- Uncaught TypeError: Cannot read property 'dataset' of null
-
-    because the script script(src='/js/mapbox.js')
- was integrated at the beginning of tour.pug while it
- really should be at the bottom of the tour.pug page
- because the DOM is not yet loaded in the beginning.
-
- -- To fix this error, we remove the script tag from tour.pug
- and put it under the footer in base.pug
-
- -- Note:  PUG files do not trigger a restart of our server
+ TODO: Wrap all map functionality into a function which will
+    be exported to index.js export function displayMap that will
+    take in array of locations that will be then be read
+    in the index.js file;
+ Note: index.js will be used to get data from the user
+    interface, so, from the website, and then will delegate
+    some actions into these other modules
  */
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const display = locations => {
+  mapboxgl.accessToken = 'Put your access token here';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'put your style mapbox cdn here',
+    scrollZoom: false,
+    // center: [-118.11349, 34.111745],
+    // zoom: 1
+    // interactive: true,
+    // trackResize: true,
+    pitch: 45
+  });
 
-console.log(locations);
+  /*
+   TODO:
+   -- Create a bound var that automatically figures out the position
+   of the map based on our tour location points
+   -- Put all the locations for a certain tour on the map and figure out which
+   portion of the map to display in order to fit all the tour coordinates.
+   -- LatLngBounds() is the area that will be displayed on the map.
+   */
+  const bounds = new mapboxgl.LngLatBounds();
 
-mapboxgl.accessToken = 'Put your access token here';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'put your style mapbox cdn here',
-  scrollZoom: false,
-  // center: [-118.11349, 34.111745],
-  // zoom: 1
-  // interactive: true,
-  // trackResize: true,
-  pitch: 45
-});
+  // Extend LatLngBounds with all the locations in our locations array
+  locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-/*
-    TODO:
-     -- Create a bound var that automatically figures out the position
-     of the map based on our tour location points
-     -- Put all the locations for a certain tour on the map and figure out which
-     portion of the map to display in order to fit all the tour coordinates.
-     -- LatLngBounds() is the area that will be displayed on the map.
- */
-const bounds = new mapboxgl.LngLatBounds();
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-// Extend LatLngBounds with all the locations in our locations array
-locations.forEach(loc => {
-  // Create marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    // Add popup to display location
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    // Extend map
+    bounds.extend(loc.coordinates);
+  });
 
-  // Add popup to display location
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
-
-  // Extend map
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};

d51f8a6ca2322c1750a027d98acf5e7d7fccb4d8
Koko-Dev <koko.webdev@gmail.com>
2021-01-29 11:25:23 -0500

    In public/js/index.js, remove the submit event listener on pages where .form does not exist to correct error
    // public/js/index.js
    // TODO: Remove submit event listener on pages where the form DNE
    const loginForm = document.querySelector('.form');
    if (loginForm) {
      loginForm.addEventListener('submit', e => {
        e.preventDefault();
    
        // Get email and password value the user puts in
        const email = document.getElementById('email').value;
        const password = document.getElementById('password').value;
        login(email, password);
      });
    }

--- public/js/index.js
+++ public/js/index.js
@@ -4,22 +4,25 @@ import { displayMap } from './mapbox';
 import { login } from './login';
 
 // DOM ELEMENTS
-// Note: We ran into some problems where an error showed up when we were not on the tour page where a map is displayed;
-// TODO: Create an if statement which checks which page we are on; if we are on a tour page, only then attempt to
-//  display a map
+// Note: We ran into some problems where an error showed up when we were
+//  not on the tour page where a map is displayed;
+// TODO: Create an if statement which checks which page we are on;
+//  if we are on a tour page, only then attempt to display a map
 const mapBox = document.getElementById('map');
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
-
-  // Call displayMap function from mapbox.js and pass in locations array
   displayMap(locations);
 }
 
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
+// TODO: Remove submit event listener on pages where the form DNE
+const loginForm = document.querySelector('.form');
+if (loginForm) {
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
 
-  // Get email and password value the user puts in
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+    // Get email and password value the user puts in
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}

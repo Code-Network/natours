62497f3be54652261aafec8eb71d6c33258e4e0e
Koko-Dev <koko.webdev@gmail.com>
2021-02-08 13:31:55 -0500

    In public/js/updateSettings.js, comment out the updateData(name, email) function and, instead, create a function called updateSettings( data, type ) which takes two parameters, { data } and 'type'; if type is 'data' then we will use Axios to make an API call to the 'Update Current User' route 'http://localhost:3000/api/v1/users/updateMe' in the form of a PATCH request with the data ({name, email }); if type is 'password' then we will use Axios to make an API call to the 'Update Current Password' route 'http://localhost:3000/api/v1/users/updateMyPassword', also with method: 'PATCH' but the data will include the current password, the new password and the new password confirm; note that the body is from the currently logged in req.user; we will use showAlert() function to send the appropriate messages depending on success
    // updateSettings.js
    export const updateSettings = async (data, type) => {
      try {
        //
        const url =
          type === 'password'
            ? 'http://localhost:3000/api/v1/users/updateMyPassword'
            : 'http://localhost:3000/api/v1/users/updateMe';
        const res = await axios({
          method: 'PATCH',
          url,
          data
        });
    
        if (res.data.status === 'success') {
          showAlert('success', `${type.toUpperCase()} was successfully updated`);
        }
      } catch (err) {
        showAlert('error', err.response.data.message);
      }
    };

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,36 +2,69 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-// TODO: Create a function that receives updated name and email from account page
-// name and email are what we want to update
-export const updateData = async (name, email) => {
+/* TODO: Create a function to update name and/or email XOR update password
+   Note: Body comes from req.user
+type = data or password
+type = 'data' = Update Current User = http://localhost:3000/api/v1/users/updateMe
+       Body = {"name": "Bee", "email": "email@email.com"}
+type = 'password' = 'Update Current Password' = http://localhost:3000/api/v1/users/updateMyPassword
+       Body = {
+         "passwordCurrent": "newpassword",
+         "password": "newpassword2",
+         "passwordConfirm": "newpassword2"
+       }
+*/
+export const updateSettings = async (data, type) => {
   try {
-    /*
-    todo:  Use Axios to create the API call itself ( import from module )
-
-    Note: Hit 'Update Current User' route {{URL}}api/v1/users/updateMe
-     which receives in the body only the data the user wants to update
-     (name/email).
-     - We get the name and email from req.user.
-     - In this route, we do not have to specify the User ID as a URL parameter
-          as we had to in the 'Update User' route
-          => {{URL}}api/v1/users/5fe25ef513a0d114fb35a6a0
-  */
+    //
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/updateMyPassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-      data: {
-        name: name,
-        email: email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} was successfully updated`);
     }
   } catch (err) {
-    // Note: This message property is the one we are defining on the server
-    //    whenever there is an error.
     showAlert('error', err.response.data.message);
   }
 };
+
+// TODO: Create a function that receives updated name and email from account page.
+// Note: name and email are what we want to update
+// export const updateData = async (name, email) => {
+//   try {
+//     /*
+//     todo:  Use Axios to create the API call itself ( import from module )
+//
+//     Note: Hit 'Update Current User' route {{URL}}api/v1/users/updateMe
+//      which receives in the body only the data the user wants to update
+//      (name/email).
+//      - We get the name and email from req.user.
+//      - In this route, we do not have to specify the User ID as a URL parameter
+//           as we had to in the 'Update User' route
+//           => {{URL}}api/v1/users/5fe25ef513a0d114fb35a6a0
+//   */
+//     const res = await axios({
+//       method: 'PATCH',
+//       url: 'http://localhost:3000/api/v1/users/updateMe',
+//       data: {
+//         name: name,
+//         email: email
+//       }
+//     });
+//
+//     if (res.data.status === 'success') {
+//       showAlert('success', 'Data updated successfully!');
+//     }
+//   } catch (err) {
+//     // Note: This message property is the one we are defining on the server
+//     //    whenever there is an error.
+//     showAlert('error', err.response.data.message);
+//   }
+// };

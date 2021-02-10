1df3d6fff14712c22e75b8897866716c5008a443
Koko-Dev <koko.webdev@gmail.com>
2021-02-10 11:28:18 -0500

    In routes/userRoutes.js, in /updateMe PATCH route, after handler userController.uploadUserPhoto, add handler userController.resizeUserPhoto; the goal of that handler will be to resize the photo into a square with a quality of 90% and as a jpeg using npm sharp
    // routes/userRoutes.js
    
    router.patch(
      '/updateMe',
      userController.uploadUserPhoto,
      userController.resizeUserPhoto,
      userController.updateMe
    );

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -47,6 +47,7 @@ router.get('/me', userController.getMe, userController.getUser);
 router.patch(
   '/updateMe',
   userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
   userController.updateMe
 );
 

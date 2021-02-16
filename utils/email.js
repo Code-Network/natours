145e4a71c767e6ab4478278eacdb6bc903738dcd
Koko-Dev <koko.webdev@gmail.com>
2021-02-16 13:55:04 -0500

    In utils/email.js, move the transporter to the Email class and create a transporter method which will distinguish between a transporter in production, which will use Sendgrid eventually and a transporter in Development, which will use MailTrap; remove the old sendEmail transporter and supporting code;  at this point, we have not removed the mailOptions, but we will integrate it into our class
    module.exports = class Email {
      constructor(user, url) {
        this.to = user.email;
        this.firstName = user.name.split(' ')[0];
        this.url = url;
        this.from = `Bree Lorenz <${process.env.EMAIL_FROM}>`;
      }
    
      // note: We want to have different transports for production and development
      // Production => Sendgrid; Development => MailTrap
      createTransport() {
        // step: If in production, we will use Sendgrid
        if (process.env.NODE_ENV === 'production') {
          // Sendgrid
          return 1;
        }
    
        // step: If in development, mail will be caught by MailTrap
        return nodemailer.createTransport({
          host: process.env.EMAIL_HOST,
          port: process.env.EMAIL_PORT,
          auth: {
            user: process.env.EMAIL_USERNAME,
            pass: process.env.EMAIL_PASSWORD
          }
        });
      }
    };

--- utils/email.js
+++ utils/email.js
@@ -23,56 +23,48 @@ module.exports = class Email {
     // step: Set Email address as an environment variable in config
     this.from = `Bree Lorenz <${process.env.EMAIL_FROM}>`;
   }
-};
 
-const sendEmail = async options => {
-  // step: 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+  // note: We want to have different transports for production and development
+  // Production => Sendgrid; Development => MailTrap
+  createTransport() {
+    // step: If in production, we will use Sendgrid
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
     }
-  });
 
-  /*
-   step: 2) Define the email options
-   note:
-    -- from: 'sender name <senderAddress@email.com>
-     -- to: "bar@example.com, baz@example.com", // list of receivers
-     -- subject: "Hello ✔", // Subject line
-     -- text: "Hello world?", // plain text body
-     -- html: "<b>Hello world?</b>", // html body
-
-     -- console.log("Message sent: %s", info.messageId);
-     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-     // Preview only available when sending through an Ethereal account
-     -- console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-   */
-  const mailOptions = {
-    from: 'Bree Lorenz <hello@bree.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
-
-  /*
-     3) step: Actually send the email with nodemailer sendMail()
-      -- sendMail() syntax from nodemailer returns a PROMISE,
-            so we will async/await instead
-
-      Mail.sendMail(
-          data: Object,
-          callback?: Function)
-      // Sends an email using the preselected transport object
-      // Params: data -- E-data description
-                 callback - Callback to run once the sending succeeded or failed
-  */
-  await transporter.sendMail(mailOptions);
+    // step: If in development, mail will be caught by MailTrap
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 };
 
-module.exports = sendEmail;
+/*
+ step: 2) Define the email options
+ note:
+  -- from: 'sender name <senderAddress@email.com>
+   -- to: "bar@example.com, baz@example.com", // list of receivers
+   -- subject: "Hello ✔", // Subject line
+   -- text: "Hello world?", // plain text body
+   -- html: "<b>Hello world?</b>", // html body
+
+   -- console.log("Message sent: %s", info.messageId);
+   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+   // Preview only available when sending through an Ethereal account
+   -- console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+ */
+const mailOptions = {
+  from: 'Bree Lorenz <hello@bree.io>',
+  to: options.email,
+  subject: options.subject,
+  text: options.message
+  // html:
+};

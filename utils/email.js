f0b25127aea37a07e95f84b67e56075e20893db0
Koko-Dev <koko.webdev@gmail.com>
2021-02-16 12:55:10 -0500

    In utils/email.js, correct error on mailOptions from: property; email could be surrounded with <>; also add comments and refactor

--- utils/email.js
+++ utils/email.js
@@ -11,9 +11,24 @@ const sendEmail = async options => {
     }
   });
 
-  // step: 2) Define the email options
+  /*
+   step: 2) Define the email options
+   note:
+    -- from: 'sender name <senderAddress@email.com>
+     -- to: "bar@example.com, baz@example.com", // list of receivers
+     -- subject: "Hello ✔", // Subject line
+     -- text: "Hello world?", // plain text body
+     -- html: "<b>Hello world?</b>", // html body
+
+     -- console.log("Message sent: %s", info.messageId);
+     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+     // Preview only available when sending through an Ethereal account
+     -- console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+   */
   const mailOptions = {
-    from: 'Bree Lorenz admin@jonas.io',
+    from: 'Bree Lorenz <hello@bree.io>',
     to: options.email,
     subject: options.subject,
     text: options.message

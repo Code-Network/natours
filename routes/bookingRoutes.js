67e4dedf354227c96407a0505393e0f185d85603
Koko-Dev <koko.webdev@gmail.com>
2021-02-19 10:43:41 -0500

    In routes/bookingRoutes.js, the goal is to create a route for the client to create a Stripe Checkout Session for the particular tour they would like to book; ensure it is a protected booking route, meaning that we will will use exports.protect from the authController.js file to ensure the user has been signed in and validated, but Stripe will handle all transaction security otherwise; this will be our way of integrating Stripe into the backend by creating an API endpoint ( '/checkout-session/:tourID' ) which will create and send back a Stripe Checkout Session
    // bookingRoutes.js
    const express = require('express');
    const bookingController = require('./../controllers/bookingController');
    const authController = require('./../controllers/authController');
    
    const router = express.Router();
    
    // TODO: Create a route for the client to create a Stripe Checkout Session
    router.get(
      '/checkout-session/:tourID',
      authController.protect,
      bookingController.getCheckoutSession
    );
    
    module.exports = router;

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,4 +4,11 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+// TODO: Create a route for the client to create a Stripe Checkout Session
+router.get(
+  '/checkout-session/:tourID',
+  authController.protect,
+  bookingController.getCheckoutSession
+);
+
 module.exports = router;

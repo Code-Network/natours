25d79555155d54fcdd1fb0b6b07ad953dc03ac9b
Koko-Dev <koko.webdev@gmail.com>
2021-03-03 10:45:29 -0500

    In models/bookingModel.js, create a pre middleware which fires when an admin or a guide queries who has booked their tour; populate the entire user document and only the tour name and call next
    // models/bookingModel.js

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -25,6 +25,21 @@ const bookingSchema = new mongoose.Schema({
   }
 });
 
+// TODO:  Populate the tour and the user whenever there is a booking query
+// Note: This is for an admin or a guide to check who has booked their tour.
+bookingSchema.pre(/^find/, function(next) {
+  // note: .populate() allows us to reference documents in outer collections
+  // This should not greatly affect performance because only admins and guides
+  //  will be able to check who has actually booked their tour.
+  // step: populate the user and the tour ( but only the name of the tour )
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name'
+  });
+
+  next();
+});
+
 const Booking = mongoose.model('Booking', bookingSchema);
 
 module.exports = Booking;

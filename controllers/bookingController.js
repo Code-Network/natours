3327888ccd9e925a380982bddc8249c916d7e77b
Koko-Dev <koko.webdev@gmail.com>
2021-03-06 10:30:01 -0500

    In controllers/bookingController.js, create a handler called deleteBooking which would enable an admin or lead-guide to delete a booking based on ID; use the deleteOne(Model) in the handlerFactory.js module
    // controllers/handlerFactory.js - deleteOne()
    exports.deleteOne = Model =>
      catchAsync(async (req, res, next) => {
        const doc = await Model.findByIdAndDelete(req.params.id);
    
        if (!doc) {
          return next(new AppError('No document found with that ID', 404));
        }
    
        res.status(204).json({
          status: 'success',
          data: null
        });
      });
    
    // ----------------------------------
    
      // controllers/bookingController.js
      exports.deleteBooking = factory.deleteOne(Booking);

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -166,3 +166,4 @@ exports.getAllBookings = factory.getAll(Booking);
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);

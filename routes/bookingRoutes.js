127ab3064b0bd2d34885cdc465ed6ebabc1ae0e0
Koko-Dev <koko.webdev@gmail.com>
2021-03-06 09:40:13 -0500

    In routes/bookingRoutes.js, the only route which a role: 'user' will have access to is the '/checkout-session/:tourId' in order to purchase a tour;  restrict all other routes to role: 'admin' and role: 'lead-guide'
    // Note: The routes below will be restricted to admin and lead-guide roles
    router.use(authController.restrictTo('admin', 'lead-guide'));

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,9 +4,13 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+// Note: All booking routes are strictly for authenticated users
 router.use(authController.protect);
 
 // TODO: Create a route for the client to create a Stripe Checkout Session
 router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+// Note: The routes below will be restricted to admin and lead-guide roles
+router.use(authController.restrictTo('admin', 'lead-guide'));
 
 module.exports = router;
